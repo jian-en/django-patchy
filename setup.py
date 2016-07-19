1f7364cfa61ebf1a9141099219e43b5e7b6cd14e
jian-en <fujian_en@126.com>
2016-07-19 19:40:33 +0800

    update setup

--- setup.py
+++ setup.py
@@ -1,5 +1,14 @@
 import os
+import re
 from setuptools import setup
+
+
+def get_version(package):
+    """
+    Return package version as listed in `__version__` in `init.py`
+    """
+    init_py = open(os.path.join(package, '__init__.py')).read()
+    return re.search("__version__ = ['\"]([^'\"]+)['\"]", init_py).group(1)
 
 
 def get_packages(package):
@@ -10,9 +19,11 @@ def get_packages(package):
             for dirpath, dirnames, filenames in os.walk(package)
             if os.path.exists(os.path.join(dirpath, '__init__.py'))]
 
+version = get_version('patchy')
 
 setup(
     name='djangopatchy',
+    version=version,
     description='Useful django patch for luojilab team',
     author='Fu Jian',
     author_email='fujian@luojilab.com',
