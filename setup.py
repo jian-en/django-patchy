3ecfc190e86bffe2e5db5c9c960ef884d858a796
jian-en <fujian_en@126.com>
2016-07-19 19:10:00 +0800

    add setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='djangopatchy',
+    description='Useful django patch for luojilab team',
+    author='Fu Jian',
+    author_email='fujian@luojilab.com',
+    zip_safe=False
+)
