305ec6258c9b2a8918bae08b5adc27e9db9a0cf6
jian-en <fujian_en@126.com>
2016-07-19 19:26:50 +0800

    update setup

--- setup.py
+++ setup.py
@@ -1,9 +1,21 @@
+import os
 from setuptools import setup
 
+
+def get_packages(package):
+    """
+    Return root package and all sub-packages.
+    """
+    return [dirpath
+            for dirpath, dirnames, filenames in os.walk(package)
+            if os.path.exists(os.path.join(dirpath, '__init__.py'))]
+
+
 setup(
-    name='luojilab_patchy_project',
+    name='djangopatchy',
     description='Useful django patch for luojilab team',
     author='Fu Jian',
     author_email='fujian@luojilab.com',
+    packages=get_packages('patchy'),
     zip_safe=False
 )
