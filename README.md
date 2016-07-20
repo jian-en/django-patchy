7cbd0e39f43e8223d5c9198d13b1ef05481c2c82
jian-en <fujian_en@126.com>
2016-07-20 14:45:28 +0800

    add the doc

---
+++ README.md
@@ -0,0 +1,31 @@
+# [Django Patchy]
+
+**Useful django app and utils to dev large-scale web app**
+
+# Requirements
+
+* Python (2.7)
+* Django (1.8, 1.9)
+
+# Installation
+
+Install using `pip`...
+
+    pip install djangopatchy
+
+# Middlewares
+
+## LongRequestMiddleware
+
+Let's add a middleware to calculate the time elapse for each request
+
+Add a middleware at the top:
+
+```python
+MIDDLEWARE_CLASSES = (
+    'patchy.middleware.LongRequestMiddleware',
+    ...
+)
+```
+
+Then for each web request, it has a header `X-ELAPSED` in seconds to indicate the time elapse. And a setting variable called `PATCHY_LONG_REQUEST_TIMEOUT` is provided. It can be set in the settings.py file. It defaults to 1 second. If it exceeds the `PATCHY_LONG_REQUEST_TIMEOUT` a error log message will be sent.
--- setup.py
+++ setup.py
@@ -3,6 +3,10 @@ import re
 from setuptools import setup
 
 
+def read_md(f):
+    return open(f, 'r', encoding='utf-8').read()
+
+
 def get_version(package):
     """
     Return package version as listed in `__version__` in `init.py`
@@ -24,9 +28,21 @@ version = get_version('patchy')
 setup(
     name='djangopatchy',
     version=version,
-    description='Useful django patch for luojilab team',
+    license='MIT License',
+    description='Useful django patch for large project',
+    long_description=read_md('README.md'),
     author='Fu Jian',
     author_email='fujian@luojilab.com',
     packages=get_packages('patchy'),
-    zip_safe=False
+    install_requires=[],
+    zip_safe=False,
+    classifiers=[
+        'Framework :: Django',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: MIT License',
+        'Operating Systems :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+    ]
 )
